0a41e4c1a7e0d06b4de0c5b31f9b40fd9ecdb1d2
Tom Cook <MintyPeterson@users.noreply.github.com>
2016-09-23 10:03:40 +0100

    Updated the formatting of Schedules.sql.

--- Schedules.sql
+++ Schedules.sql
@@ -7,13 +7,13 @@ GO
 GO
 
 CREATE TABLE [Schedules] (
-	[ScheduleID] bigint NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	[IsEnabled] bit NOT NULL,
-	[StartDate] date NOT NULL,
-	[EndDate] date,
-	[FrequencyType] smallint NOT NULL,
-	[FrequencyInterval] smallint NOT NULL,
-	[FrequencyRelativeInterval] smallint NOT NULL,
-	[FrequencyRecurrenceFactor] smallint NOT NULL
+  [ScheduleID] bigint NOT NULL IDENTITY(1, 1) PRIMARY KEY
+ ,[IsEnabled] bit NOT NULL
+ ,[StartDate] date NOT NULL
+ ,[EndDate] date
+ ,[FrequencyType] smallint NOT NULL
+ ,[FrequencyInterval] smallint NOT NULL
+ ,[FrequencyRelativeInterval] smallint NOT NULL
+ ,[FrequencyRecurrenceFactor] smallint NOT NULL
 )
 ;
